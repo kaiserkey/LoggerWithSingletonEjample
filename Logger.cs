14b525ed4fb5b596cc06095d7f433650aacfae3b
Daniel <daniel.gonzalez@beclevercorp.com>
2025-01-15 16:46:51 -0300

    logger with singleton pattern

---
+++ Logger.cs
@@ -0,0 +1,34 @@
+﻿namespace TestSingleton
+{
+    /// <summary>
+    /// Guardar los en archivo en disco
+    /// </summary>
+    public sealed class Logger
+    {
+        private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance => _instance.Value;
+
+        private object _lock = new object();
+
+        //obtener el path actual
+        private static string Path = null;
+
+        private Logger(){
+            Path = Directory.GetCurrentDirectory() + "\\..\\..\\..\\Log.txt";
+            this.LogInformation("Inicio del Logger.");
+        }
+
+        public void LogInformation(string message)
+        {
+            lock (_lock)
+            {
+                using (var writer = new StreamWriter(Path, true))
+                {
+                    writer.WriteLine($"LogInformation - {DateTime.Now} - {message}");
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestSingleton;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Logger.Instance.LogInformation("Test Logger");
+        Logger.Instance.LogInformation("Test Logger");
+
+        var logger = Logger.Instance;
+        logger.LogInformation("Test Logger");
+
+        //var logger2 = new Logger(); // error por constructor privado
+        Console.WriteLine("Logger instanciado correctamente");
+        Console.WriteLine("Fin del programa");
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+    }
+}
